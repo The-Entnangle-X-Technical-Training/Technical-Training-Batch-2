ec451ae3aeea59747612e2256e0cae3236df7165
varchit76-crypto <varchit76@gmail.com>
2026-03-27 12:44:38 +0530

    Update problem18.py

--- archit_vishwakarma/Level-1/01-Basics/problem18.py
+++ archit_vishwakarma/Level-1/01-Basics/problem18.py
@@ -1,2 +1,14 @@
 problem18 = 'hello 18th problem'
-print(problem18)
+print(problem18)#  Average of Marks in 5 Subjects
+
+math = int(input('Enter math marks : '))
+eng = int(input('Enter English marks : '))
+gk = int(input('Enter GK marks : '))
+hindi = int(input('Enter Hindi marks : '))
+science = int(input('Enter science marks : '))
+
+total_marks = math + eng +gk + hindi + science
+
+avg_marks = total_marks/5
+
+print(f"Total makrs obtained : {total_marks}\nAverage marks : {avg_marks}")
