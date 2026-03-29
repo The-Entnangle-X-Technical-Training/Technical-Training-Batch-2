9708f2b40f78636a5168150d41d6a997e88d4602
varchit76-crypto <varchit76@gmail.com>
2026-03-29 08:16:43 +0530

    update

--- archit_vishwakarma/Level-1/02-Conditional/02-problem3.py
+++ archit_vishwakarma/Level-1/02-Conditional/02-problem3.py
@@ -4,9 +4,3 @@ num2 = int(input("Enter 2nd number : "))
 greater = f"{num1} is greater" if num1>num2 else f"{num2} is greater"
 
 print(greater)
-# if num1 > num2:
-#     print(f"{num1} > {num2}")
-# else:
-#     print(f"{num2} > {num1}")
-
-#there is an exceprion in this code check
