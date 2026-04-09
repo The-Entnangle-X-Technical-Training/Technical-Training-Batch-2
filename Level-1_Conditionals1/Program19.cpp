2cc3f40998350ca95e504d12f098c7615a846490
nkrutika <128682742+nkrutika@users.noreply.github.com>
2026-04-09 20:46:26 +0530

    Added Program 19 in Conditionals

---
+++ Level-1_Conditionals1/Program19.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  int num ;
+    cout << " Enter a number : ";
+    cin >> num ;
+    
+    if (num <= 9 )
+    cout << " Single Digit " ;
+    
+    else 
+    cout << " Not a Single Digit " ;
+    
+    return 0 ;
+}
