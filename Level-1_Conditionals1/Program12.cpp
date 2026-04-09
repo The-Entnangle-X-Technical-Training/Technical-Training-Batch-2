a108c94c30e92a09221b53a7f4951794d7e5c10e
nkrutika <128682742+nkrutika@users.noreply.github.com>
2026-04-09 20:40:36 +0530

    Added Program 12 in Conditionals

---
+++ Level-1_Conditionals1/Program12.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  int N ;
+    cout << " Enter a Number ";
+    cin >> N ;
+    if (N % 3 == 0) {
+      cout << " Multiple of 3";
+    }
+
+    else {
+    cout << " Not a multiple of 3" ;
+    }
+    return 0 ;
+}
