ab1a049728e6127965228ad88b1be08d85810df0
nkrutika <128682742+nkrutika@users.noreply.github.com>
2026-04-09 23:16:08 +0530

    Added Program 17 in Patterns

---
+++ Level-1_Patterns/Program17.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  int N ;
+  cout << "Enter size of square: " ;
+  cin >> N;
+  
+  for (int i=1; i<=N; i++) {
+    for (int j=1; j<=N; j++) {
+      if (i==1 || i==N || j==1|| j==N)
+        cout<< "*" ;
+      else 
+        cout << " " ;
+    }
+    cout << endl ;
+  }
+
+    return 0;
+}
