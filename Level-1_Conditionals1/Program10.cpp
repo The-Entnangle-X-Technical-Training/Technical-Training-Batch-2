803fdd93d63221c84a114d7ec72dbff11a90beb0
nkrutika <128682742+nkrutika@users.noreply.github.com>
2026-04-09 20:39:10 +0530

    Added Program 10 in Conditionals

---
+++ Level-1_Conditionals1/Program10.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2 ;
+    
+    cout << "Enter First Number : " ;
+    cin >> num1 ;
+    cout << " Enter Second Number : " ;
+    cin >> num2 ;
+    if (num1 > num2) {
+      cout << " Second Number is Smaller " ;
+    }
+    
+    else 
+    cout << " First Number is Smaller " ;
+
+    return 0;
+}
