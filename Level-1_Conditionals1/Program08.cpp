b2b404032f48b22eecd805740cdbc3306a4101e6
nkrutika <128682742+nkrutika@users.noreply.github.com>
2026-04-09 20:37:51 +0530

    Added Program 8 in Conditionals

---
+++ Level-1_Conditionals1/Program08.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num ;
+    
+    cout << "Enter a Number : " ;
+    cin >> num ;
+    if (num % 5 == 0) {
+      cout << " Divisible by 5 " ;
+    }
+    
+    else 
+    cout << " Not Divisible by 5 " ;
+
+    return 0;
+}
