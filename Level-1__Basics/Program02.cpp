e74a775c37ebad2ffbb0aad98e5126cd08bd3977
nkrutika <128682742+nkrutika@users.noreply.github.com>
2026-04-09 20:03:04 +0530

    Added basic Program 2

---
+++ Level-1__Basics/Program02.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int num1, num2, sum;
+    cout << "Enter First Number: ";
+    cin >> num1;
+    cout << "Enter Second Number: ";
+    cin >> num2;
+    sum = num1 + num2;
+    cout << "The sum of these two numbers is: " << sum << endl;
+    return 0;
+}
