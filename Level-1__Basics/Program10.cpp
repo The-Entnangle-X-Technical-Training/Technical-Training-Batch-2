112dabe92c7c65b8d94bd04ab0d32ebaa7be0322
nkrutika <128682742+nkrutika@users.noreply.github.com>
2026-04-09 20:10:34 +0530

    Added Basic Program 10

---
+++ Level-1__Basics/Program10.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    double length, width, area ;
+    cout << " Enter the length of the rectangle : " ;
+    cin >> length ;
+    cout << " Enter the width of the rectangle : " ;
+    cin >> width ;
+    area = length * width ;
+    cout << " Area of the given rectangle = " << area << endl ;
+    return 0;
+}
