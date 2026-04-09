65edb646c666988c537ad0677aa26ab4c554c6d6
nkrutika <128682742+nkrutika@users.noreply.github.com>
2026-04-09 23:28:19 +0530

    Added Program 3 in Arrays

---
+++ Level-1_Arrays/Program03.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  int arr[5], sum=0 ;
+    cout << "Enter 5 numbers: ";
+    for (int i=0; i <5; i++) {
+      cin >> arr[i] ;
+    }
+    cout << "Sum of all Array Elements:" ;
+    for (int i =0; i<5; i++) {
+    sum = sum+ arr[i] ;
+    }
+    
+    cout << sum << endl;
+    return 0;
+}
