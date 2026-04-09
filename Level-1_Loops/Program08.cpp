32d61ad16bd792ca097343c2051c318664ac5fd7
nkrutika <128682742+nkrutika@users.noreply.github.com>
2026-04-09 22:54:39 +0530

    Added Program 8

---
+++ Level-1_Loops/Program08.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int sum=0 ;
+  
+  for (int i =1; i <=10; i++) {
+  sum = sum+i ;
+}
+  cout<< "Sum of Numbers from 1-10: " << sum << endl;
+    return 0;
+}
