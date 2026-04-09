314c20e7096d445bff1906e6322cc7db567b763d
nkrutika <128682742+nkrutika@users.noreply.github.com>
2026-04-09 20:53:14 +0530

    Added Program 1 in Conditionals

---
+++ Level-1_Conditionals2/Program01.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int num  ;
+    cout << "Enter a Number : " ;
+    cin >> num ;
+    
+    if (num > 0) {
+     cout << " Positive Number " ;
+    }
+    else if (num < 0) {
+     cout << " Negative Number " ;
+    }
+    else {
+      cout << " Zero " ;
+    }
+    return 0 ;
+}
