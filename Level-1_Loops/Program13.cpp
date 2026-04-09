36a0d03b6e8682912a581b6f49d431c320896edc
nkrutika <128682742+nkrutika@users.noreply.github.com>
2026-04-09 22:57:05 +0530

    Added Program 13

---
+++ Level-1_Loops/Program13.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  
+  cout << "Multiples of 5 between 1-50" ;
+  for (int i =1; i <=50; i++) {
+   if( i % 5 == 0)
+   cout << i << endl;
+}
+    return 0;
+}
