e8fbd4655b9e464271e14790be527f6c8c94eba7
nkrutika <128682742+nkrutika@users.noreply.github.com>
2026-04-09 23:00:04 +0530

    Added Program 19

---
+++ Level-1_Loops/Program19.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int square, count =0 ;
+  cout << "Squares of First 10 Natural Numbers: " << endl;
+  
+  for (int i =1; i <=10; i++) {
+    cout << i << "² = " << i * i << endl;
+}
+    return 0;
+}
