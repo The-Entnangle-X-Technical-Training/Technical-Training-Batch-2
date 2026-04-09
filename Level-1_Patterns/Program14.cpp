ac375e8a25f45f6a134b950495d7a65937597d0d
nkrutika <128682742+nkrutika@users.noreply.github.com>
2026-04-09 23:14:20 +0530

    Added Program 14 in Patterns

---
+++ Level-1_Patterns/Program14.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  int N ;
+  
+    for (int i=1; i<=4; i++) {
+      for (int j=1; j <= 4; j++) {
+        cout <<  i*j << " " ;
+      }
+      cout << endl;
+    }
+    return 0;
+}
