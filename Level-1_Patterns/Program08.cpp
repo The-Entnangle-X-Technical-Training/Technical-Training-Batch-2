bc8da2d34d4833f784ff254b09493146a7333b24
nkrutika <128682742+nkrutika@users.noreply.github.com>
2026-04-09 23:09:56 +0530

    Added Program 8 in Patterns

---
+++ Level-1_Patterns/Program08.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  int N ;
+  cout << "Enter no. of rows: " ;
+  cin >> N ;
+  
+    for (int i=1; i<=N; i++) {
+      for (int j=N; j >= i; j--) {
+        cout << "*" ;
+      }
+      cout << endl;
+    }
+    return 0;
+}
