2153e81b1ca1ca80dc3e445d5693b0914a77e1d0
nkrutika <128682742+nkrutika@users.noreply.github.com>
2026-04-09 21:01:58 +0530

    Added Program 13 in Conditionals

---
+++ Level-1_Conditionals2/Program13.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double num1, num2, num3 ;
+    
+    cout << "Enter First Number: " ;
+    cin >> num1 ;
+    cout << "Enter Second Number: " ;
+    cin >> num2 ;
+    cout << "Enter Third Number: " ;
+    cin >> num3 ;
+    
+    if (num1 > num2 && num1 > num3) {
+        cout << " First number is the Largest -> " << num1 << endl ;
+    } 
+    
+    else if (num2 > num1 && num2 > num3) {
+        cout << " Second number is the Largest -> " << num2 << endl ;
+    }
+    
+    else {
+      cout << " Third Number is the Largest -> " << num3 << endl ;
+    }
+
+    return 0;
+}
