b646a671af20c40f9851024cb4578518181f8afd
nkrutika <128682742+nkrutika@users.noreply.github.com>
2026-04-09 23:08:42 +0530

    Added Program 6 in Patterns

---
+++ Level-1_Patterns/Program06.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  int N ;
+  cout << "Enter no. of rows: " ;
+  cin >> N ;
+  
+    for (int i=1; i <= N; i ++) {
+        cout << "*" ;
+    }
+    return 0;
+}
