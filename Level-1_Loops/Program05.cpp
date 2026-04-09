4397a6f03be384b56fdc7fa4f948bfdfe829a14d
nkrutika <128682742+nkrutika@users.noreply.github.com>
2026-04-09 22:53:07 +0530

    Added Program 5

---
+++ Level-1_Loops/Program05.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "First 10 Even Numbers : " ;
+  for (int i =1; i <=20; i++) {
+    if (i % 2 != 0)
+    cout << i << endl;
+}
+
+    return 0;
+}
