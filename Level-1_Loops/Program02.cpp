a754d0459f7918b0134a34fd22dd0508662de308
nkrutika <128682742+nkrutika@users.noreply.github.com>
2026-04-09 22:51:23 +0530

    Added Program 2

---
+++ Level-1_Loops/Program02.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int N;
+  cout << "Enter a Number: " ;
+  cin >> N ;
+  cout << "Numbers from 1 to N: " ;
+  for (int i =1; i <=N; i++) {
+    cout << i << endl;
+}
+
+    return 0;
+}
