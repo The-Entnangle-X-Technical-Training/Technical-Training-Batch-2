c3d60deae03e39fa36b1707e876109cdde5f3626
nkrutika <128682742+nkrutika@users.noreply.github.com>
2026-04-09 20:39:48 +0530

    Added Program 11 in Conditionals

---
+++ Level-1_Conditionals1/Program11.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  int N ;
+    cout << " Enter a Number ";
+    cin >> N ;
+    if (N >= 1 && N <= 10 ) {
+      cout << " Yes ";
+    }
+
+    else {
+    cout << " No " ;
+    }
+    return 0 ;
+}
