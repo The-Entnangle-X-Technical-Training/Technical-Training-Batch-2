46bb08efa2af38d16e58a16449d882cce45095b6
nkrutika <128682742+nkrutika@users.noreply.github.com>
2026-04-09 20:48:18 +0530

    Added Program 22 in Conditionals

---
+++ Level-1_Conditionals1/Program22.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int cost, sell  ;
+    cout << "Enter Cost Price : " ;
+    cin >> cost ;
+    cout << "Enter Selling Price : " ;
+    cin >> sell ;
+    
+    if (cost > sell ) {
+     cout << " Loss " ;
+    }
+    else if ( cost < sell ) {
+      cout << " Profit " ;
+    }
+    else {
+    cout << " No Profit. No Loss. " ;
+    }
+    
+    return 0 ;
+}
